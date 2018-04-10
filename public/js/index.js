41f897c985cbde8f10a349ff546a2e03c0e203cd
Dawid Bijak <bijak.dawid@gmail.com>
2018-04-10 21:14:23 +0200

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -27,14 +27,16 @@ socket.on('newLocationMessage', function(message) {
     $('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function(e){
+$('#message-form').on('submit', function(e){
     e.preventDefault();
+
+    var messageTextbox =$('[name=message]');
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-        console.log('ACK');
+        messageTextbox.val('');
     });
 });
 
@@ -45,12 +47,16 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending...');
+
     navigator.geolocation.getCurrentPosition(function (position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
